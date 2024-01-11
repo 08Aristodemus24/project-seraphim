7f4e34d2b6d64048b9c1deb0d2434e814e927eb4
08Aristodemus24 <MichaelAveuc571@gmail.com>
2024-01-11 09:45:00 +0800

    update

--- client-side/src/components/Form.svelte
+++ client-side/src/components/Form.svelte
@@ -155,13 +155,14 @@
         <input bind:value={seq_len} type="number" id="seq-len" class="seq-len-field" placeholder="250"/>
     </div>
     <div class="image-upload-container">
-        <label for="image-upload" class="image-upload-label">Image</label>
-        <input type="file" id="image-upload" class="image-upload-field" bind:files={images}>
-        
         <!-- when image is uploaded images becomes are not null anymore
         but when another upload occurs and is cancelled images becomes
         a list of length 0 -->
         <img class="uploaded-image" src={images != null ? images.length != 0 ? URL.createObjectURL(images[0]) : null : null} alt="">
+        <div class="image-upload-field-wrapper">
+            <label for="image-upload" class="image-upload-label">Image</label>
+            <input type="file" id="image-upload" class="image-upload-field" bind:files={images}>
+        </div>
     </div>
 
     <button type="submit" class="submit-btn">Submit</button>
